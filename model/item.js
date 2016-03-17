788ed8cf91d5714923c39f2e93afd1c694dc809e
ctevnan <carolyntevnan@yahoo.com>
2016-03-16 21:43:02 -0400

    made item,js. will work on adding comment and user.js tomorrow. changed title in index.html

---
+++ model/item.js
@@ -0,0 +1,28 @@
+var Schema = mongoose.Schema;
+
+/* Item is an obj in a collection*/
+/* Items belong to User*/
+/*Items has many Comments*/
+
+/*_userOwned is an object, refs a User // find owner */
+
+/* itemComments is an array of ids, refs comment schema, populatable //related comments*/
+
+var itemSchema = new Schema ({
+  _owner: { type: Schema.Types.ObjectId, ref:'User'},
+
+  itemName: String,
+  itemDescription: String,
+  itemPrice: Number,
+  itemSold: Boolean,
+  comments: [{
+    type: Schema.Types.ObjectId,
+    ref: 'comment'
+  }]
+});
+
+/*each item thats for sale has an owner which has the objectId of a user*/
+/*comments is ann array of objectIds which are comments*/
+
+var Item = mongoose.model('Item', itemSchema);
+module.exports= Item;
