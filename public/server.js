478970eec0fb6ad219145b28bec84f93858924a8
ctevnan <carolyntevnan@yahoo.com>
2016-03-16 20:17:17 -0400

    setting up folders and files, getting server set up, getting connection to db set up

---
+++ public/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+var mongojs = require('mongojs');
+var db = mongojs('yardsale', ['items']);
+db.on('error', function(err) {
+  console.log('database error:', err);
+})
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.send(index.html);
+})
+
+app.listen(PORT, function() {
+  console.log('Listening on port: ' + PORT);
+})
