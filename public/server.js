8c554e49728c6eee70a4b74e1a910a816732e3aa
ctevnan <carolyntevnan@yahoo.com>
2016-03-17 14:31:01 -0400

    adding user to server.js, still not spinning up

--- public/server.js
+++ public/server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var bodyParser = require('bodyParser');
+var mongoose = require('mongoose');
 var app = express();
 var PORT = 3000;
 var mongojs = require('mongojs');
@@ -9,10 +11,29 @@ db.on('error', function(err) {
 
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.get('/', function (req, res) {
+  res.render('index', {
+    msg: req.query.msg
+  });
+});
+
+app.post('/register', function (req. res) {
+  User.create (req.body).then(function(user) {
+    req.session.authenticated = user;
+    res.redirect('/secret');
+  }).catch(function(err) {
+    res.redirect('/?msg' + err.message);
+  });  
+});
+
 app.get('/', function (req, res) {
   res.send(index.html);
 })
 
-app.listen(3000, function() {
-  console.log('Listening on port: ' + PORT);
-})
+app.listen(PORT, function() {
+  console.log('Listening on port %s', + PORT);
+});  
