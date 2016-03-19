607920dd8ab2ef2773d0c44e3f2d6be712dc5cd4
ctevnan <carolyntevnan@yahoo.com>
2016-03-18 22:06:37 -0400

    added code to userSchema to have the date added to created_at for first saves and to updated_at on every save. left a note to myself to hash passwords here

--- model/user.js
+++ model/user.js
@@ -13,5 +13,22 @@ var userSchema = new Schema({
 //schema is useless w/o a model to use it
 var User = mongoose.model('User', userSchema);
 
+// on every save, add the date
+userSchema.pre('save', function(next) {
+  // get current date
+  var currentDate = new Date();
+
+  //channge the updated_at field to current date
+  this.updated_at = currentDate;
+
+  //if created_at doesnt exist, add it 
+  if (!this.created_at)
+    this.created_at = currentDate;
+
+  next();
+});
+
+//hash passwords. dont save in plaintext  
+
 //making this accessable to users
 module.exports = User;
