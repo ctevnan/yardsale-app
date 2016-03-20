47eedb3f296c1a16df17911fd5e62fc2097a9447
ctevnan <carolyntevnan@yahoo.com>
2016-03-20 16:54:53 -0400

    adding routes

--- server.js
+++ server.js
@@ -1,27 +1,48 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+//var logger = require('morgan');
 var app = express();
-var mongojs = require('mongojs');
-var db = mongojs('yardsale', ['items']);;
 var PORT = process.env.NODE_ENV || 3000;
+
+/*mongoose connection*/
+var db = 'mongodb://localhost/items';
+mongoose.connect(db);
 
 db.on('error', function(err) {
   console.log('database error:', err);
 })
-
 db.on('connection', function() {
   console.log('connection to database made');
 });
 
-app.use(express.static('public'));
+var Item = require('./Item');
 
+//app.use(logger('dev'));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
 app.get('/', function (req, res) {
-  res.send(process.cwd() + "index.html");
+ res.send(index.html);
+});
+
+app.get('/items', function (req, res) {
+  Item.find(function (err, docs) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      res.send(docs);
+    }
+  });
+}};
+
+app.post('/register', function (req, res) {
+  var firstname = req.body.firstname;
+  var lastname = req.body.lastname;
 })
 
 app.post('/login', function (req, res) {
