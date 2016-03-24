d4fd0954f063de8633f2d04fc98dcb9a24c9af7c
ctevnan <carolyntevnan@yahoo.com>
2016-03-24 15:26:49 -0400

    adding  method, etc

--- controller/app.js
+++ controller/app.js
@@ -7,5 +7,19 @@ angular.module('yardsaleApp', [])
   })
 
   itemList.addItem = function() {
-    console.log
+    console.log("adding item");
+    var newItem = {
+      itemName: itemList.name,
+      itemDescription: itemList.description,
+      itemPrice: itemList.price,
+      itemSold: itemList.sold,
+      comments: itemList.comments,
+    };
+    $http({
+      method: 'POST',
+      url: '/newItem',
+      data: newItem,  
+    }).then(function (result) {
+      itemList.items.push(result.data);
+    })
   }
